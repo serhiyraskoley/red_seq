3e9e416eae36c16963f0178340663c765f4947a1
Raskoley <Serhiy.Raskoley@SEBN.COM>
2023-07-05 13:51:48 +0300

    Import request and add value and key

--- red_seq.py
+++ red_seq.py
@@ -1,11 +1,26 @@
 import requests
+import json
 
-headers = {"X-Redmine-API-Key": "9751d3c925b7bece550d2ba310aa1d3667295655"}
+HEADER = {"X-Redmine-API-Key": "9751d3c925b7bece550d2ba310aa1d3667295655"}
 URL = "http://192.168.154.128/issues/34233.json"
 
-request = requests.get(URL, headers=headers)
+data = {
+	"issue": {
+		"project_id": 118,
+		"tracker_id": 88,
+		"subject": "Hello world"
+	}
+}
 
-if request.status_code == 200:
-    print(request.json())
-else:
-    print(f"Error: {request.status_code}")
+DATA = json.dumps(data)
+
+print(DATA)
+
+
+
+# request = requests.get(URL, headers=HEADER)
+
+# if request.status_code == 200:
+#     print(request.json())
+# else:
+#     print(f"Error: {request.status_code}")
